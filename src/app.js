0765983da2231f4fbb037fdcd2479156236bd6cc
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-09-21 11:00:53 -0500

    asdfads

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ class App {
         this.config();
         this.routes.routes(this.app);
         this.mongoSetup();
+        
         shelljs.exec('apt-get update', { async: true });
         shelljs.exec('apt-get install xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2', { async: true });
         shelljs.exec('npm install cypress', { async: true });
