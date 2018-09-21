7dc2dfbd033c23ecb07abde2171d870dbd1192af
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-09-21 10:37:22 -0500

    asdfasdf

--- src/models/report.js
+++ src/models/report.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const shelljs = require("shelljs");
 const mongoose = require("mongoose");
 const fs = require('file-system');
 const cypress = require('cypress');
@@ -45,35 +46,37 @@ class Report {
     }
     executeScreenshotsGenerator() {
         const currentReport = this;
-        cypress.run({
-            spec: 'cypress/integration/colors_spec.js',
-            env: {
-                id: currentReport._id
-            }
-        }).then(() => {
-            const preFileName = `generador-${currentReport._id}-pre.png`;
-            const postFileName = `generador-${currentReport._id}-post.png`;
-            currentReport.baseImage = fs.readFileSync(`./cypress/screenshots/colors_spec.js/${preFileName}`);
-            currentReport.modifiedImage = fs.readFileSync(`./cypress/screenshots/colors_spec.js/${postFileName}`);
-            resemble(`./cypress/screenshots/colors_spec.js/${preFileName}`)
-                .compareTo(`./cypress/screenshots/colors_spec.js/${postFileName}`)
-                .ignoreLess()
-                .onComplete(function (data) {
-                currentReport.diffImage = data.getBuffer();
-                currentReport.diffInformation.misMatchPercentage = data.misMatchPercentage;
-                currentReport.diffInformation.isSameDimensions = data.isSameDimensions;
-                currentReport.diffInformation.dimensionDifference = data.dimensionDifference;
-                const ReportModel = mongoose.model('Report', exports.ReportSchema);
-                ReportModel.findById(currentReport._id, (err, report) => {
-                    report.executed = true;
-                    report.baseImage = currentReport.baseImage;
-                    report.modifiedImage = currentReport.modifiedImage;
-                    report.diffImage = currentReport.diffImage;
-                    report.diffInformation = currentReport.diffInformation;
-                    report.save();
-                });
+        shelljs.exec('npm install cypress').then(() => {
+            cypress.run({
+                spec: 'cypress/integration/colors_spec.js',
+                env: {
+                    id: currentReport._id
+                }
+            }).then(() => {
+                const preFileName = `generador-${currentReport._id}-pre.png`;
+                const postFileName = `generador-${currentReport._id}-post.png`;
+                currentReport.baseImage = fs.readFileSync(`./cypress/screenshots/colors_spec.js/${preFileName}`);
+                currentReport.modifiedImage = fs.readFileSync(`./cypress/screenshots/colors_spec.js/${postFileName}`);
+                resemble(`./cypress/screenshots/colors_spec.js/${preFileName}`)
+                    .compareTo(`./cypress/screenshots/colors_spec.js/${postFileName}`)
+                    .ignoreLess()
+                    .onComplete(function (data) {
+                        currentReport.diffImage = data.getBuffer();
+                        currentReport.diffInformation.misMatchPercentage = data.misMatchPercentage;
+                        currentReport.diffInformation.isSameDimensions = data.isSameDimensions;
+                        currentReport.diffInformation.dimensionDifference = data.dimensionDifference;
+                        const ReportModel = mongoose.model('Report', exports.ReportSchema);
+                        ReportModel.findById(currentReport._id, (err, report) => {
+                            report.executed = true;
+                            report.baseImage = currentReport.baseImage;
+                            report.modifiedImage = currentReport.modifiedImage;
+                            report.diffImage = currentReport.diffImage;
+                            report.diffInformation = currentReport.diffInformation;
+                            report.save();
+                        });
+                    });
             });
-        });
+        }); 
     }
 }
 exports.Report = Report;
